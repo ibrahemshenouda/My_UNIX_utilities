c0b8376ea95d3ebbaac1cf2569fb10e42d8f9aff
ibrahemshenouda <91206576+ibrahemshenouda@users.noreply.github.com>
2025-04-03 12:32:16 +0200

    Update mypwd.c

--- mypwd.c
+++ mypwd.c
@@ -1,30 +1,42 @@
+/*******includes Section***********/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
+/*******Mcros Section*************/
 #define COUNT 1024
-int main(int argc, char* argv[argc])
+
+int main(int argc, char** argv)
 {
-        if(argc != 1)
+    /*checking for arguments*/
+    if(argc != 1)
+    {
+        printf("Usage: %s\n", argv[0]);
+        exit(-1);
+    }
+    /*creat buffer to store path in this buffer*/
+    char buf[COUNT];
+    /*checking for getced() reurn value*/
+    /*@buf: char array to store path, 
+     @COUNT: number of bytes to store in buf*/
+    if(getcwd(buf, COUNT) != NULL)
+    {
+        /*checking for write() return value*/
+        /*1 : to write in stdout 'terminal'
+         buf: data write() will print in stdout
+         strlen(buf): buffer actual size reurned from getcwd()
+        */
+        if(write(1, buf, strlen(buf)) < 0)
         {
-                printf("Usage: %s\n",argv[0]);
-                exit(-1);
+            printf("error: can not write in stdout\n");
+            exit(-2);
         }
-        char buf[COUNT];
-        if(getcwd(buf, COUNT) != NULL)
-        {
-                if(write(1, buf, strlen(buf)) < 0)
-                {
-                        printf("error: cant write in stdout\n");
-                        exit(-2);
-                }
-                printf("\n");
-        }
-        else
-        {
-                printf("error: cant read current path\n");
-                exit(-3);
-        }
-        return 0;
+        printf("\n");
+    }
+    else
+    {
+        printf("error: can not read path\n");
+        exit(-3);
+    }
+    return 0;
 }
